bbc02618ca537a65da7ec65e6f762143e38b59d5
ntducnhan1996 <ntducnhan1996@gmail.com>
2023-02-14 10:36:40 +0700

    commit all first time

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.scss';
+import Index from './pages/Index';
+
+
+function App() {
+  return (
+    <div className="App">
+        <Index/>
+
+    </div>
+  );
+}
+
+export default App;
